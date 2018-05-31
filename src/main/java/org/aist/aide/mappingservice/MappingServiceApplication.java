499e99e2be4c83ceea4d5b2e31cded5cdd5ed034
Gabriel Perez Clark <gaboperezclark@gmail.com>
2018-05-31 14:35:35 -0400

    Added histrix and cleaned application.yml

--- src/main/java/org/aist/aide/mappingservice/MappingServiceApplication.java
+++ src/main/java/org/aist/aide/mappingservice/MappingServiceApplication.java
@@ -2,9 +2,11 @@ package org.aist.aide.mappingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 public class MappingServiceApplication {
     public static void main(String[] args) {
